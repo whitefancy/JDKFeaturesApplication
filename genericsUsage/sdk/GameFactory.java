68f4222d781c3d2569579b8bd5fce402e179f953
whitefancy <whitefancy@msn.cn>
2021-12-22 23:04:10 +0800

    抽象工厂与反射结合使用

--- genericsUsage/sdk/GameFactory.java
+++ genericsUsage/sdk/GameFactory.java
@@ -15,6 +15,8 @@ public class GameFactory {
         }
         return null;
     }
+//    知识点 抽象工厂
+//    知识点，多个泛型的使用，泛型方法的使用，通过泛型的类型，来获得实例
     public  static <T,Q> SDKProxy<T,Q> getInstance(Class<T> cls1,Class<Q> cls2){
         return new SDKProxy<T, Q>() {
             T newT = null;
@@ -36,6 +38,7 @@ public class GameFactory {
                     e.printStackTrace();
                 }
                 try {
+//                    知识点，调用类的静态方法，obj可以传null，也可以传实例
                     getName.invoke(null);
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
@@ -48,9 +51,8 @@ public class GameFactory {
             public T getInstance() {
 
                 try {
+//                    知识点：通过类中变量名，来获得变量的值
                     Field field = newQ.getClass().getField("instance");
-                    //设置对象的访问权限，保证对private的属性的访问
-
                     return  (T)field.get(newQ);
                 } catch (Exception e) {
 
@@ -68,6 +70,7 @@ public class GameFactory {
                 }
                 String account = null;
                 try {
+//                    知识点:带有返回值的方法调用
                      account= (String) getName.invoke(newT);
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
@@ -76,6 +79,8 @@ public class GameFactory {
                 }
                 Method call= null;
                 try {
+//                    知识点，需要使用反射调用的方法，必须是public公开的
+//                    知识点：带有参数的方法调用
                     call = newQ.getClass().getMethod("callback",String.class);
                 } catch (NoSuchMethodException e) {
                     e.printStackTrace();
@@ -111,6 +116,55 @@ public class GameFactory {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        try {
+            String channel="Channel2";
+            String gameType="Unity";
+//            知识点:通过类名获得类的类型
+            Class<?> helperClazz1 = Class.forName("JDKFeaturesApplication.genericsUsage.sdk."+channel+"Helper");
+            Class<?> callbackClazz1 = Class.forName("JDKFeaturesApplication.genericsUsage.sdk."+gameType+"Callback");
+            SDKProxy game5=GameFactory.getInstance(helperClazz1,callbackClazz1);
+            game5.init();
+            game5.login();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        SDKProxy game6=GameFactory.getGame("Channel2","Unity");
+        game6.init();
+        game6.login();
+        SDKProxy game7=GameFactory.getGame(channel.Channel2,gameType.Cocos);
+        game7.init();
+        game7.login();
+    }
 
+    private static SDKProxy getGame(channel achannel, gameType atype) {
+        return getGame(achannel.name(),atype.name);
+    }
+
+    static SDKProxy getGame(String channel,String gameType){
+        try{
+            Class<?> helperClazz1 = Class.forName("JDKFeaturesApplication.genericsUsage.sdk."+channel+"Helper");
+            Class<?> callbackClazz1 = Class.forName("JDKFeaturesApplication.genericsUsage.sdk."+gameType+"Callback");
+            SDKProxy game=GameFactory.getInstance(helperClazz1,callbackClazz1);
+            return game;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+//    知识点 Enum与String的转化
+    enum gameType{
+        Cocos("Cocos"),Unity("Unity");
+
+        private final String name;
+
+        gameType(String name) {
+            this.name=name;
+        }
+    }
+    enum channel{
+        Local("Local"),Channel2("Channel2");
+
+        channel(String channel) {
+        }
     }
 }
